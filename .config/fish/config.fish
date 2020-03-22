2f74c9241d4d5c284b5fff0700f751b88d379541
Jeff Gough <j@jeffreygough.com>
2020-03-22 22:42:54 +0000

    Switched to using Fish as my shell. Configured it to use the Powerline prompt

---
+++ .config/fish/config.fish
@@ -0,0 +1,7 @@
+set fish_function_path $fish_function_path "/usr/share/powerline/fish"
+source /usr/share/powerline/fish/powerline-setup.fish
+powerline-setup
+
+function config
+	command /usr/bin/git --git-dir=/home/jeff/.cfg/ --work-tree=/home/jeff $argv
+end
