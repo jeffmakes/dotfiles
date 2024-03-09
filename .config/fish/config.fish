28b74bd50b86b50f6b2c0e254d9065ba08017317
Jeff Gough <j@jeffreygough.com>
2024-03-09 12:14:16 +0000

    Add the no-tracking flag to stop CLI apps sending telemetry data

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,4 +10,7 @@ set --export PATH $HOME/.local/bin $HOME/bin /var/lib/snapd/snap/bin /opt/ti/msp
 set --export PYTHONPATH /usr/lib64/kicad-nightly/lib64/python3.10/site-packages $PYTHONPATH
 set --export LD_LIBRARY_PATH /usr/lib64/kicad-nightly/lib64 $LD_LIBRARY_PATH
 set --export EDITOR nvim
+set --export DO_NOT_TRACK 1
 
+alias ding='aplay ~/tools/soft/win95-sounds/DING.WAV >/dev/null 2>&1'
+alias tada='aplay ~/tools/soft/win95-sounds/TADA.WAV >/dev/null 2>&1'
